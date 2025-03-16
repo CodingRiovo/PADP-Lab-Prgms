b73bbdbe2ba9e834c8ec710e7be4be46e638ef56
CodingRiovo <codingriovo@gmail.com>
2025-03-16 21:55:21 +0530

    change few programs for mistakes and reworks

--- PADP Lab/Program_7/prog7.c
+++ PADP Lab/Program_7/prog7.c
@@ -9,34 +9,27 @@
 #define MAX_ITER 1000
 #define TOL 1e-6
 
-void initialise(double u[N][M],double f[N][M])
-{
-    for(int i=0;i<N;i++)
-    {
-        for(int j=0;j<M;j++)
-        {
+void initialize(double u[N][M],double f[N][M]){
+    for(int i=0;i<N;i++){
+        for(int j=0;j<M;j++){
             u[i][j] =0.0;
             f[i][j]=1.0;      
         }
     }
 }
 
-double jacobi(double u[N][M],double f[N][M])
-{
+double jacobi(double u[N][M],double f[N][M]){
     double u_new[N][M];
     double max_diff=0;
     int iter=0;
 #pragma acc data copyin(f[0:N][0:M]) copy(u[0:N][0:M]) create(u_new[0:N][0:M])
-    do
-    {
+    do{
         max_diff=0.0;
 
         #pragma acc parallel loop reduction(max:max_diff) collapse(2)
 
-        for(int i=1;i<N-1;i++)
-        {
-            for(int j=1;j<M-1;j++)
-            {
+        for(int i=1;i<N-1;i++){
+            for(int j=1;j<M-1;j++){
                 u_new[i][j]=0.25*(u[i-1][j]+u[i+1][j]+u[i][j-1]+u[i][j+1]-f[i][j]);
                 double diff=fabs(u[i][j]-u_new[i][j]);
                 if(diff>max_diff)
@@ -46,10 +39,8 @@ double jacobi(double u[N][M],double f[N][M])
 
         #pragma acc parallel loop collapse(2)
 
-        for(int i=0;i<N;i++)
-        {
-            for(int j=0;j<M;j++)
-            {
+        for(int i=0;i<N;i++){
+            for(int j=0;j<M;j++){
                 u[i][j]=u_new[i][j];
             }
         }
@@ -61,12 +52,11 @@ double jacobi(double u[N][M],double f[N][M])
 
 }
 
-int main()
-{
+int main(){
     double(*u)[M]=calloc(N,sizeof(*u));
     double(*f)[M]=calloc(N,sizeof(*f));
 
-    initialise(u,f);
+    initialize(u,f);
 
     double t = omp_get_wtime();
     double max=jacobi(u,f);
--- PADP Lab/test.c
+++ PADP Lab/test.c
@@ -1,52 +1,61 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<gd.h>
+#include<math.h>
 #include<omp.h>
+#include<openacc.h>
 
-int main(int argc, char* argv[]){
-    if(argc != 3){
-        printf("Usage: %s <input.png> <output.png>\n", argv[0]);
-        exit(1);
-    }
+#define M 1000
+#define TOL 1e-6
+#define MAX_ITER 1000
 
-    FILE* ifp = fopen(argv[1], "rb");
-    FILE* ofp = fopen(argv[2], "wb");
-    gdImagePtr img = gdImageCreateFromPng(ifp);
-    int w = gdImageSX(img);
-    int h = gdImageSY(img);
-
-    printf("Size: %d x %d pixels\n", w, h);
-
-    gdImagePtr outImg = gdImageCreateTrueColor(w, h);
-    omp_set_num_threads(4);
-    double t = omp_get_wtime();
-
-    #pragma omp parallel for schedule(guided, 25)
-    for(int x=0; x<w; x++){
-        for(int y=0; y<h; y++){
-            int color = gdImageGetPixel(img, x, y);
-            int tid = omp_get_thread_num();
-            int avgColor = (gdImageRed(img, color)+gdImageGreen(img, color)+gdImageBlue(img, color))/3;
-
-            switch(tid){
-                case 0:
-                    color = gdImageColorAllocate(outImg, avgColor, 0, 0);
-                    break;
-                case 1:
-                    color = gdImageColorAllocate(outImg, 0, avgColor, 0);
-                    break;
-                case 2:
-                    color = gdImageColorAllocate(outImg, 0, 0, avgColor);
-                    break;
-                default:
-                    color = gdImageColorAllocate(outImg, avgColor, avgColor, avgColor); 
-            }
-            gdImageSetPixel(outImg, x, y, color);
+void initialize(double u[M][M], double f[M][M]){
+    for(int i=0; i<M; i++){
+        for(int j=0; j<M; j++){
+            u[i][j] = 0.0;
+            f[i][j] = 1.0;
         }
     }
+}
 
-    gdImagePng(outImg, ofp);
+double jacobi(double u[M][M], double f[M][M]){
+    double u_new[M][M];
+    double max_diff;
+    int iter = 0;
+
+    #pragma acc data copyin(f[0:M][0:M]) copy(u[0:M][0:M]) create(u_new[0:M][0:M])
+    do{
+        max_diff = 0;
+        #pragma acc parallel loop reduction(max:max_diff) collapse(2)
+        for(int i=1; i<M-1; i++){
+            for(int j=1; j<M-1; j++){
+                u_new[i][j] = 0.25*(u[i-1][j] + u[i+1][j] + u[i][j-1] + u[i][j+1] - f[i][j]);
+                double diff = fabs(u_new[i][j] - u[i][j]);
+                if(diff > max_diff)
+                    max_diff = diff;
+            }
+        }
+
+        #pragma acc parallel loop collapse(2)
+        for(int i=0; i<M; i++){
+            for(int j=0; j<M; j++){
+                u[i][j] = u_new[i][j];
+            }
+        }
+
+        iter++;
+    }while(iter<MAX_ITER && max_diff > TOL);
+    return max_diff;
+}
+
+int main(){
+    double(*u)[M] = calloc(M, sizeof(*u));
+    double(*f)[M] = calloc(M, sizeof(*f));
+
+    initialize(u, f);
+    double t = omp_get_wtime();
+    double ans = jacobi(u, f);
     t = omp_get_wtime() - t;
-    printf("Time taken: %f\n", t);
+
+    printf("Max Diff: %f, Time: %f\n", ans, t);
     return 0;
 }
